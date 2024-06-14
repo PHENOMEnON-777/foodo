6b7ae3b89e4dfc936688a0d2d40a4cebaac5806d
INANIS <noumbiap.anthony@ictuniversity.edu.cm>
2024-06-14 07:53:20 +0100

    1

--- back-end/app.js
+++ back-end/app.js
@@ -14,7 +14,18 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   next();
 });
+const allowedOrigins = ['https://your-netlify-site.netlify.app'];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
 
+app.use(cors(corsOptions));
 app.post('/users', async (req, res) => {
   try {
     console.log('Request body:', req.body);
